76549637139a2d06a585219484c1992b3fe13045
Dmitry Buryak <b.dmitry@oggettoweb.com>
2013-02-28 13:48:26 +0400

    chainable class

---
+++ Chainable.php
@@ -0,0 +1,56 @@
+<?php
+
+class Chainable
+{
+    private $instance;
+    private $returns;
+
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $args = func_get_args();
+        $obj  = array_shift($args);
+        $this->instance = is_object($obj) ? $obj : new $obj($args);
+        $this->__resetReturns();
+    }
+
+    /**
+     * PHP magic mathod
+     *
+     * @param string $method method name
+     * @param array  $args   method arguments
+     * @return Chainable
+     */
+    public function __call($method, $args)
+    {
+        $this->returns[$method] = call_user_func_array(array($this->instance, $method), $args);
+        return $this;
+    }
+
+    /**
+     * Set value of last called method to given variable
+     *
+     * @param mixed &$var variable
+     * @return Chainable
+     */
+    public function __getReturn(&$var)
+    {
+        $var = array_pop($this->returns);
+        return $this;
+    }
+
+    /**
+     * Reset $returns propery
+     *
+     * @return Chainable
+     */
+    public function __resetReturns()
+    {
+        $this->returns = array();
+        return $this;
+    }
+}
